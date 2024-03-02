7f5204b93ce873970e219b2bf1c59f6e0ac61f69
Chris Sexton <sextonch@oregonstate.edu>
2024-03-02 17:40:28 -0500

    Homepage Resort class

--- Homepage.py
+++ Homepage.py
@@ -1,58 +1,60 @@
 import requests
 
 
-class Resort():
+class Resort:
     """
     Description:
     """
+    def __init__(self, city_name):
+        """XXX"""
+        self._weather_data = None
+        while self._weather_data is None:
+            self._weather_data = self._get_weather_data(city_name)
+        self._city = city_name
+        self._temperature = temperature = self._weather_data['main']['temp']
+        self._description =  self._weather_data['weather'][0]['description']
+        self._humidity = self._weather_data['main']['humidity']
+        self._speed = self._weather_data['wind']['speed']
 
+    def _get_weather_data(self, city_name):
+        """XXX"""
+        base_url = "http://api.openweathermap.org/data/2.5/weather"
 
+        # Set up the parameters for the API request
+        api_key = '9f6930e1d6505ed136bd2863a790dab8'
+        params = {
+            'q': city_name,
+            'appid': api_key,
+        }
 
-def get_weather(api_key, city_name):
-    """
-    Description:
-    """
-    base_url = "http://api.openweathermap.org/data/2.5/weather"
+        # Make the API call
+        response = requests.get(base_url, params=params)
 
-    # Set up the parameters for the API request
-    params = {
-        'q': city_name,
-        'appid': api_key,
-    }
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            weather_data = response.json()
+            return weather_data
+        else:
+            print(f"Error: Unable to fetch weather data. Status Code: Try again.")
+            return None
 
-    # Make the API call
-    response = requests.get(base_url, params=params)
+    def get_city(self):
+        """XXX"""
+        return self._city
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON response
-        weather_data = response.json()
-        return weather_data
-    else:
-        print(f"Error: Unable to fetch weather data. Status Code: {response.status_code}")
-        return None
+    def get_temperature(self):
+        """XXX"""
+        return self._temperature
 
+    def get_description(self):
+        """XXX"""
+        return self._description
 
-def main():
-    api_key = '9f6930e1d6505ed136bd2863a790dab8'
+    def get_humidity(self):
+        """XXX"""
+        return self._humidity
 
-    # Input the city name
-    city_name = input("Enter the city name: ")
-
-    # Get weather data
-    weather_data = get_weather(api_key, city_name)
-
-    # Display the result
-    if weather_data:
-        city_name = city_name
-        temperature = weather_data['main']['temp']
-        description = weather_data['weather'][0]['description']
-        humidity = weather_data['main']['humidity']
-        speed = weather_data['wind']['speed']
-    else:
-        print("Failed to retrieve weather information.")
-        return
-
-
-if __name__ == "__main__":
-    main()
+    def get_speed(self):
+        """XXX"""
+        return self._speed
