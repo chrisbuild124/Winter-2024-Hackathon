4b62bd45551cfeb7d19c065b4cf364dd9c0fadc7
elliot <chegyourface@gmail.com>
2024-03-03 14:43:30 -0800

    Updated for resorts

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template
-from resort import Resort
+from flask import Flask, render_template, request
+from .resorts import Resorts
 
 app = Flask(__name__)
 
-@app.route('/')
+api_key= "9f6930e1d6505ed136bd2863a790dab8"
 
 # Create instances of the Resort class
-resort1 = Resort(name="Breckenridge", city="Breckenridge, Colorado", api_key=api_key)
-resort2 = Resort(name="Kirkwood", city="Kirkwood, California", api_key=api_key)
+resort1 = Resorts(city_name="Breckenridge, Colorado")
+resort2 = Resorts(city_name="Kirkwood, California")
 
-resort1.get_weather()
-resort2.get_weather()
+resort1._get_weather_data(city_name="Breckenridge, Colorado")
+resort2._get_weather_data(city_name="Kirkwood, California")
 
+resorts = [resort1, resort2]
+
+@app.route('/', methds=['GET', 'POST'])
 def index():
-    return render_template('index.html', resorts=[resort1, resort2])
+    if request.method == 'POST':
+        city = request.form['city']
+
+        new_resort = Resorts(city_name=city)
+        new_resort.get_weather()
+        resorts.append(new_resort)
+    
+    return render_template('index.html', resorts=resorts)
 
 if __name__ == '__main__':
     app.run(debug=True)
